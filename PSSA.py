a5cb69fb9b5e39072ae7468fbba0e11e3dad87d5
Pranshu Trivedi <ptrivedi@lawn-143-215-97-98.lawn.gatech.edu>
2019-12-02 19:59:10 -0500

    Skeleton code for PSSA

---
+++ PSSA.py
@@ -0,0 +1,21 @@
+
+#import needed classes
+
+
+class PSSA:
+    """
+        initialize needed parameters
+    """
+
+    def __init__(self,  filtered_df, mood_vector, playlist_duration):
+        self.filtered_df = filtered_df
+        self.mood_vector = mood_vector
+        self.playlist_duration = playlist_duration
+
+
+    """
+        Main PSSA Algorithm
+    """
+    def run(self):
+        pass
+
