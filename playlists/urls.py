0d73df9518e487c1ba2237c816a785a0bcc7aa8d
Pranshu Trivedi <ptrivedi@lawn-143-215-97-98.lawn.gatech.edu>
2019-12-03 03:56:42 -0500

    Added generate url

--- playlists/urls.py
+++ playlists/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from playlists.views import SearchSongs, PlayListDetail
+from playlists.views import SearchSongs, PlayListDetail, Filtering
 
 
 
 urlpatterns = [
     path('songs/', SearchSongs.as_view() ,name='search_results'),
-    path('playlists/', PlayListDetail.as_view(), name='playlist_details')
+    path('playlists/', PlayListDetail.as_view(), name='playlist_details'),
+    path('generate/', Filtering.as_view(), name='playlist_generator')
 ]
