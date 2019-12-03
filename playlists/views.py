a15b02d248aec9b1aced6dfd86cf532f34d26d81
Pranshu Trivedi <ptrivedi@lawn-143-215-97-98.lawn.gatech.edu>
2019-12-03 03:57:31 -0500

    Added generate view

--- playlists/views.py
+++ playlists/views.py
@@ -8,6 +8,9 @@ from playlists.serializers import PlaylistSerializer
 from rest_framework import status
 from rest_framework.response import Response
 import json
+import pandas as pd
+from playlists.filtering import Filter
+from PSSA import PSSA
 
 # Create your views here.
 
@@ -28,7 +31,7 @@ class PlayListDetail(APIView):
                                user=thisUser)
         for song_id in data['songs']:
             print(song_id)
-            thisSong = Song.objects.get(spotify_id=song_id)
+            thisSong = Song.objects.filter(spotify_id=song_id).first()
             newPlaylist.songs.add(thisSong)
         print("CREATED PLAYLIST")
         return Response(status=status.HTTP_201_CREATED)
@@ -44,11 +47,39 @@ class Filtering(APIView):
 
     def post(self, request):
         data = json.loads(request.body.decode('utf-8'))
+        #parameters:
         username = request.user.username
         thisUser = User.objects.get(username=username)
         thisPlaylist = Playlist.objects.get(user=thisUser)
-        songQuerySet = thisPlaylist.objects.all()
-        
+        songQuerySet = thisPlaylist.songs.all()
+        songsDF = pd.DataFrame(list(songQuerySet.values()))
+        filter = Filter(songsDF)
+        filteredDF = filter.run()
+
+        #print(filteredDF.head())
+
+
+        vsv = data['vsv']
+        vse = data['vse']
+        vev = data['vev']
+        vee = data['vee']
+        duration = data['duration']
+
+        vector_start = (vsv, vse)
+        vector_end = (vev,vee)
+
+        pssaObj = PSSA(songsDF, filteredDF, vector_start, vector_end, duration)
+        songList, artistList, genreList, VList, Elist = pssaObj.run()
+        outDict = {'songList':songList,
+                   'artistList':artistList,
+                   'genereList':genreList,
+                   'VList':VList,
+                   'EList':Elist}
+        serialized = json.dumps(outDict)
+        return Response(outDict)
+
+
+
 
 
 
